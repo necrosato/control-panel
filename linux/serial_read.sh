d6be14ec8ee44aea05fa79d891ae9e5b2724283f
Naookie Sato <naookie.sato1@hp.com>
2019-07-24 16:31:30 -0700

    logging command time

--- linux/serial_read.sh
+++ linux/serial_read.sh
@@ -7,6 +7,6 @@ echo "Reading from serial device: $SERIAL_DEVICE"
 while read -r line; do
   # Remove trailing whitespace
   line="$(echo -e "$line" | sed -e 's/[[:space:]]$//')"
-  echo "$line"
+  echo "$(TZ="America/Los_Angeles" date) - $line"
   $line
 done < <(tail -f $SERIAL_DEVICE)
