9c0e136aea7c367b3f0a69d97e04b39f22e2f6a1
Naookie Sato <naookie.sato1@hp.com>
2019-07-24 13:53:45 -0700

    inheritance, added switch

--- linux/serial_read.sh
+++ linux/serial_read.sh
@@ -5,5 +5,8 @@ SERIAL_DEVICE=$(find /dev/ -iname "TTYACM*")
 echo "Reading from serial device: $SERIAL_DEVICE"
 
 while read -r line; do
+  # Remove trailing whitespace
+  line="$(echo -e "$line" | sed -e 's/[[:space:]]$//')"
+  echo "$line"
   $line
 done < <(tail -f $SERIAL_DEVICE)
