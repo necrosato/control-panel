11c10b6f7bb1707500547301eab38184b89fb7ce
Naookie Sato <naookie.sato1@hp.com>
2019-07-25 09:33:08 -0700

    properly init serial port

--- linux/serial_read.sh
+++ linux/serial_read.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 
-SERIAL_DEVICE=$(find /dev/ -iname "TTYACM*")
+SERIAL_DEVICE=$(find /dev/ -iname "ttyACM*")
+BAUD=9600
+
+if [[ "$SERIAL_DEVICE" == "" ]]; then
+  echo "No serial device found."
+  exit 1
+fi
+
+stty -F $SERIAL_DEVICE $BAUD -icanon min 0
+
 echo "Reading from serial device: $SERIAL_DEVICE"
 
 while read -r line; do
